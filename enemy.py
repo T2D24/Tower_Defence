bab2daf9a9a8ebf97ce775da5cac1f10b4bc3abb
staylazy <47147510+staylazy@users.noreply.github.com>
2020-02-19 14:14:55 +0700

    health bar

--- enemy.py
+++ enemy.py
@@ -6,13 +6,19 @@ from levels import *
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, image):
         super(Enemy, self).__init__()
-        self.image = pygame.image.load(image)
-        self.image = pygame.transform.scale(self.image, SIZE)
+        self.pic = pygame.image.load(image)
+        self.pic = pygame.transform.scale(self.pic, (SIZE[0], SIZE[1]))
+        self.pic.convert_alpha()
+        self.image = pygame.Surface((SIZE[0], SIZE[1] + 18), 
+                            pygame.SRCALPHA | pygame.HWSURFACE)
+        self.image.blit(self.pic, (0, 18))
         self.image.convert_alpha()
-        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.pic.get_rect()
         self.rect.x = x
-        self.rect.y = y
+        self.rect.y = y 
         self.hp = 30
+        self.hp_fix = self.hp
         self.vel_x = 3
         self.path = level1
         self.vel_y = 3
@@ -20,7 +26,9 @@ class Enemy(pygame.sprite.Sprite):
     def update(self):
         self.dead()
         self.moving()
-
+        self.health_bar()
+        # print(self.rect.x, " ", self.rect.y)
+        
     def moving(self):       # переписать этот метод, а то это полное говно
         '''for dot in self.path:
             while True:
@@ -55,3 +63,11 @@ class Enemy(pygame.sprite.Sprite):
     def dead(self):
         if self.hp <= 0:
             self.kill()
+
+    def health_bar(self):
+        height = 10 
+        width = (self.hp * self.rect.width) / self.hp_fix
+        pygame.draw.line(self.image, (200, 0, 0), (0, 0), (self.rect.width, 0), height)
+        pygame.draw.line(self.image, (0, 200, 0), (0,0), (width, 0), height)
+
+
